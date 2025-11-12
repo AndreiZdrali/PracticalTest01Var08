464ffed4bce9ed0ce63ecbd0bd80658d054781a1
Andrei Zdrali <andreizdrali@gmail.com>
2025-11-12 15:06:07 +0200

    logica activitate 2 si intents

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var08/PracticalTest01Var08MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var08/PracticalTest01Var08MainActivity.java
@@ -1,9 +1,11 @@
 package ro.pub.cs.systems.eim.practicaltest01var08;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -16,6 +18,8 @@ public class PracticalTest01Var08MainActivity extends AppCompatActivity {
     private Button playButton;
     private EditText riddleText;
     private EditText answerText;
+    private TextView lastResultText;
+    private int lastResultInt = -10;
 
     private PlayButtonClickListener playButtonClickListener = new PlayButtonClickListener();
     private class PlayButtonClickListener implements View.OnClickListener {
@@ -25,7 +29,10 @@ public class PracticalTest01Var08MainActivity extends AppCompatActivity {
             var answer = riddleText.getText().toString();
 
             if (!riddle.isEmpty() && !answer.isEmpty()) {
-                Toast.makeText(getApplicationContext(), "NU SUNT GOALE", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), PracticalTest01Var08PlayActivity.class);
+                intent.putExtra("RIDDLE", riddle);
+                intent.putExtra("ANSWER", answer);
+                startActivityForResult(intent, 108);
             }
         }
     }
@@ -46,7 +53,31 @@ public class PracticalTest01Var08MainActivity extends AppCompatActivity {
         playButton = findViewById(R.id.play_button);
         riddleText = findViewById(R.id.riddle_text);
         answerText = findViewById(R.id.answer_text);
+        lastResultText = findViewById(R.id.lastresult_text);
 
         playButton.setOnClickListener(playButtonClickListener);
+
+        lastResultInt = Integer.parseInt(lastResultText.getText().toString());
+
+        if (lastResultInt != -10) {
+            if (lastResultInt == RESULT_OK) {
+                Toast.makeText(this, "Victory", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Fail", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == 108) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Victory", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Fail", Toast.LENGTH_LONG).show();
+            }
+        }
+        lastResultText.setText(resultCode);
     }
 }
--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var08/PracticalTest01Var08PlayActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var08/PracticalTest01Var08PlayActivity.java
@@ -1,6 +1,12 @@
 package ro.pub.cs.systems.eim.practicaltest01var08;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class PracticalTest01Var08PlayActivity extends AppCompatActivity {
+    private TextView riddleText;
+    private EditText answerText;
+    private Button checkButton;
+    private Button backButton;
+    private String correctAnswer;
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == R.id.check_button) {
+                var givenAnswer = answerText.getText().toString();
+                if (givenAnswer.toLowerCase().strip() == correctAnswer) {
+                    setResult(RESULT_OK, null);
+                } else {
+                    setResult(RESULT_CANCELED, null);
+                }
+            }
+            if (view.getId() == R.id.back_button) {
+                setResult(RESULT_CANCELED, null);
+            }
+            finish();
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +51,23 @@ public class PracticalTest01Var08PlayActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        riddleText = findViewById(R.id.riddle_text);
+        answerText = findViewById(R.id.answer_text);
+        checkButton = findViewById(R.id.check_button);
+        backButton = findViewById(R.id.check_button);
+
+        checkButton.setOnClickListener(buttonClickListener);
+        backButton.setOnClickListener(buttonClickListener);
+
+        Intent intent = getIntent();
+        if (intent != null && intent.getExtras().containsKey("RIDDLE") && intent.getExtras().containsKey("ANSWER")) {
+            var riddle = intent.getStringExtra("RIDDLE");
+            correctAnswer = intent.getStringExtra("ANSWER");
+
+            riddleText.setText(riddle);
+        } else {
+            Toast.makeText(this, "invalid intent", Toast.LENGTH_LONG).show();
+        }
     }
 }
